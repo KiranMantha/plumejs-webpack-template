d414c1834f28262d8e8a027a16ba618bc1fe694f
KiranMantha <kiranv.mantha@gmail.com>
2024-10-08 11:58:03 +0530

    upgraded to latest eslint and plumejs versions

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import { Component, html } from '@plumejs/core';
 import styles from './styles/base.scss';
 
-console.log('styles', styles);
+@Component({
+  selector: 'test-ele'
+})
+export class TestElement {
+  render() {
+    return html`<div data-testid="test-ele">
+      <p>i'm child element</p>
+      <p></p>
+    </div>`;
+  }
+}
 
 @Component({
-    selector: 'app-root',
-    styles: styles,
-    root: true
+  selector: 'app-root',
+  styles: styles,
+  root: true
 })
 export class AppComponent {
-    render() {
-        return html`
-            <main class='container center'>
-                <img src="./images/logo.jpg" />
-                <h1>Welcome to PlumeJS</h1>
-                <p>Please check <a href="https://github.com/KiranMantha/plumejs">here</a> for documentation</p>
-            </main>
-        `;
-    }
+  render() {
+    return html`
+      <main class="container center">
+        <img src="./images/logo.jpg" />
+        <h1>Welcome to PlumeJS</h1>
+        <p>Please check <a href="https://github.com/KiranMantha/plumejs">here</a> for documentation</p>
+        <test-ele></test-ele>
+      </main>
+    `;
+  }
 }
